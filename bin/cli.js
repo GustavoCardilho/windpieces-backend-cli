72bf5b3661665d9f061a38a71fff87f5b3fc477c
Kyoudan <fogoste@gmail.com>
2023-09-25 12:32:14 -0300

    feat: adding new options

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { execSync } from "child_process";
 
 const runCommand = (command) => {
     try {
@@ -12,19 +14,142 @@ const runCommand = (command) => {
     return true;
 };
 
-const repoName = process.argv.slice(2);
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/Kyoudan/windpieces-backend-starter.git ${repoName}`;
-const installDeps = `cd ${repoName} && npm install`;
+let repoName;
+let isInstallPrisma = false;
+let databaseWithPrisma = "";
+let packageManager = "";
 
-console.log("Creating new project...");
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(1);
+async function DefineRepoName() {
+    const res = await inquirer.prompt([
+        {
+            type: "input",
+            name: "repoName",
+            message: "What is the name of your project?",
+            default: "my-app",
+        },
+    ]);
+    repoName = res.repoName;
 
-console.log("Installing dependencies...");
-const depsInstalled = runCommand(installDeps);
-if (!depsInstalled) process.exit(1);
+    if (!res.repoName) {
+        console.error(chalk.red("Please specify the project directory:"));
+        process.exit(1);
+    }
+}
 
-console.log("Done!");
-console.log(`
-    cd ${repoName} && npm run dev
-`);
+async function addPrisma() {
+    const res = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "prisma",
+            message: "Would you like to add Prisma to your project?",
+            default: false,
+        },
+    ]);
+    isInstallPrisma = res.prisma;
+
+    if (res.prisma) {
+        const res = await inquirer.prompt([
+            {
+                type: "list",
+                name: "database",
+                message: "What database would you like to use?",
+                choices: ["PostgreSQL", "MySQL", "MongoDB", "SQLite"],
+                default: "PostgreSQL",
+            },
+        ]);
+        databaseWithPrisma = res.database.toLowerCase();
+    }
+}
+
+async function DefinePackageManager() {
+    const res = await inquirer.prompt([
+        {
+            type: "list",
+            name: "packageManager",
+            message: "What package manager do you use?",
+            choices: ["npm", "yarn", "pnpm"],
+            default: "npm",
+        },
+    ]);
+    packageManager = res.packageManager;
+}
+
+function installProject() {
+    const gitCheckoutCommand = `git clone --depth 1 https://github.com/Kyoudan/windpieces-backend-starter.git ${repoName}`;
+    let installDeps;
+
+    /*     switch (packageManager) {
+        case "yarn":
+            installDeps = `cd ${repoName} && yarn`;
+            break;
+        case "pnpm":
+            installDeps = `cd ${repoName} && pnpm install`;
+            break;
+        default:
+            installDeps = `cd ${repoName} && npm install`;
+            break;
+    } */
+
+    console.log(chalk.green("Creating new project..."));
+    console.clear();
+    const checkedOut = runCommand(gitCheckoutCommand);
+    if (!checkedOut) process.exit(1);
+
+    console.log(chalk.green("Installing dependencies..."));
+
+    if (isInstallPrisma) {
+        let installPrisma;
+
+        if (packageManager == "yarn" || packageManager == "pnpm") {
+            installPrisma = `cd ${repoName} && ${packageManager} add prisma @prisma/client -D --ignore-workspace-root-check`;
+        } else {
+            installPrisma = `cd ${repoName} && npm install prisma @prisma/client --save-dev --ignore-workspace-root-check`;
+        }
+
+        const prismaDependencies = runCommand(installPrisma);
+        if (!prismaDependencies) process.exit(1);
+
+        const prismaInstalled = runCommand(
+            `cd ${repoName} && npx prisma init --datasource-provider ${databaseWithPrisma}`
+        );
+        if (!prismaInstalled) process.exit(1);
+    }
+    console.clear();
+    console.log("Done!");
+
+    let initCommand;
+
+    if (packageManager == "yarn" || packageManager == "pnpm") {
+        initCommand = `cd ${chalk.green(repoName)} && ${packageManager} dev`;
+    } else {
+        initCommand = `cd ${repoName} && npm run dev`;
+    }
+    console.log("-".repeat(50) + "\n");
+    console.log("Your project is ready!");
+    console.log(`use ${initCommand}`);
+    console.log(
+        `consult the ${chalk.green(
+            "package.json"
+        )} file to check all available scripts \n`
+    );
+
+    console.log(
+        `supoort me on github: ${chalk.green(
+            "https://github.com/Kyoudan/windpieces-backend-cli"
+        )}\n\n`
+    );
+
+    console.log("Happy coding! 💖\n");
+
+    console.log("-".repeat(50));
+}
+
+async function setup() {
+    console.clear();
+    await DefineRepoName();
+    await addPrisma();
+    await DefinePackageManager();
+    installProject();
+}
+
+setup();
